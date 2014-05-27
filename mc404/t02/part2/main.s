0b27b7c91b664346439afbde70fce427d6312159
Hugo Kiyodi Oshiro <ra146446@gomes.lab.ic.unicamp.br>
2014-05-27 18:46:41 -0300

    mc404

--- mc404/t02/part2/main.s
+++ mc404/t02/part2/main.s
@@ -1,41 +1,224 @@
 .globl main
 
 .data
-        msg1:           .asciz          "Digite uma mensagem: "
-        msg2:           .asciz          "\nA mensagem digitada eh: "
-        mensagem:       .space          100
+    msg0:        .asciz  "\nEnter command: "
+    msg:           .asciz          "\nA mensagem digitada eh: "
+    newLine:    .asciz  "\n"
+    endMessage:    .asciz  "\nProgram End\n"
 
 .text
 main:
-        push {r7, lr}
+    push {r0}
+    ldr r0, =msg0
+    bl print_string
+    pop {r0}
 
-        @ $> Digite uma mensagem: 
-        mov r0, #0x1
-        ldr r1, =msg1
-        mov r2, #0x15
-        mov r7, #0x4
-        svc 0   
+    push {lr}
+    bl arm_getline
+    
+    push {r0}
+    ldr r0, =msg
+    bl print_string
+    pop {r0}
 
-        @ Le a mensagem do usuário
-        mov r0, #0x0
-        ldr r1, =mensagem
-        mov r2, #0x64
-        mov r7, #0x3
-        svc 0 
+    push {r0}
+    bl print_string
+    pop {r0}
+    
+    push {r0}
+    ldr r0, =newLine
+    bl print_string
+    pop {r0}
 
-        @ $> A mensagem digitada eh:
-        mov r0, #0x1
-        ldr r1, =msg2
-        mov r2, #0x19
-        mov r7, #0x4
-        svc 0
+    
+    push {r0}
+    bl arm_strtok
+    pop {r0}
+    
+    push {r0}
+    ldr r0, =endMessage
+    bl print_string
+    pop {r0}
+    
+    pop {pc}
+    
+    
+get_command:
+    push {r4, r5, r6, lr}
+    
+    push {r0, r1}
+    bl arm_getline              @line in r0
+    mov r2, r0                  @line in r2
+    pop {r0, r1}
+    
+    push {r0, r1, r2}
+    mov r0, r2                  @puts line in r0 to call arm_strtok 
+    bl arm_strtok               @strings in 'cmd' 'arg1' 'arg2' 
+    pop {r0, r1, r2}
+    
+    push {r0-r3}
+    bl print_command
+    pop {r0-r3}
+    
+    ldr r3, =cmd
+    ldr r3, [r3]                @r3 points to cmd string
+    
+    push {r0, r1, r2, r3}
+    mov r0, r3                  @put r3 in r0 to call arm_strlen
+    bl arm_strlen
+    mov r4, r0                  @puts len in r4
+    pop {r0, r1, r2, r3}
+    
+    cmp r4, #1
+    beq match_first_char
+    
+    mov r6, #1                  @counter
+    match_command:
+        ldr r4, =quit
+        loop_command:
+        
+        cmp r6, #7
+        beq loop_command_end
+        
+        push {r0-r3}
+        mov r0, r3
+        mov r1, r4
+        bl arm_strcmp
+        mov r5, r0
+        pop {r0-r3}
+        
+        cmp r5, #0
+        beq match_first_char
+        
+        push {r0-r3}
+        mov r0, r4
+        bl arm_strlen
+        mov r5, r0
+        add r5, r5, #1
+        push {r0-r3}
+        
+        add r4, r4, r5
+        
+        add r6, r6, #1
+        
+        b loop_command
+        
+        loop_command_end:
+        mov r3, #-5
+        b get_command_return
+    match_command_end:
+    
+    match_first_char:
+        ldrb r3, [r3]
+        cmp r3, #'q'
+            b treat_quit
+        cmp r3, #'s'
+            b treat_step
+        cmp r3, #'c'
+            b treat_continue
+        cmp r3, #'w'
+            b treat_write
+        cmp r3, #'p'
+            b treat_print
+        cmp r3, #'r'
+            b treat_registers
+        b default
+        
+        treat_quit:
+            mov r3, #0
+            b store_args
+        treat_quit_end:
+        
+        treat_step:
+            ldr r3, =arg1
+            ldr r3, [r3]
+            push {r0-r2}
+            mov r0, r3
+            bl arm_strtou
+            mov r3, r0
+            pop {r0-r2}
+            b store_args
+        treat_step_end:
+        
+        treat_continue:
+            mov r3, #-1
+            b store_args
+        treat_continue_end:
+        
+        treat_write:
+            mov r3, #-2
+            b store_args
+        treat_write_end:
 
-        @ Escreve a mensagem de volta na tela
-        mov r0, #0x1
-        ldr r1, =mensagem
-        mov r2, #0x64   
-        mov r7, #0x4
-        svc 0
+        treat_print:
+            mov r3, #-3
+            b store_args
+        treat_print_end:
+        
+        treat_registers:
+            mov r3, #-4
+            b store_args
+        treat_registers_end:
 
-        mov r0, #0x0 @retorno da main
-        pop {r7,pc}
+        default:
+            mov r3, #-5
+            b store_args
+        default_end:
+        
+    match_first_char_end:
+    
+    store_args:
+        @copy arg1 to r0 address
+        ldr r4, =arg1
+        ldr r4, [r4]
+        
+        push {r0-r3}
+        mov r0, r4              @puts arg1 in r0 to call strlen
+        bl arm_strlen
+        mov r5, r0              @agr1 len in r5
+        pop {r0-r3}
+        
+        push {r0-r3}
+        mov r0, r4              @puts arg1 in r0 to call strlen
+        bl arm_strlen
+        mov r5, r0              @agr1 len in r5
+        pop {r0-r3}
+        
+        
+        push {r0-r3}
+        @mov r0, r0             @r0 already contains dest
+        mov r1, r4              @puts src in r1
+        mov r2, r5              @puts len to be copy in r2        
+        bl arm_memcpy
+        pop {r0-r3}
+        
+        @copy arg2 to r0 address
+        ldr r4, =arg2
+        ldr r4, [r4]
+        
+        push {r0-r3}
+        mov r0, r4              @puts arg1 in r0 to call strlen
+        bl arm_strlen
+        mov r5, r0              @agr1 len in r5
+        pop {r0-r3}
+        
+        push {r0-r3}
+        mov r0, r4              @puts arg1 in r0 to call strlen
+        bl arm_strlen
+        mov r5, r0              @agr1 len in r5
+        pop {r0-r3}
+        
+        push {r0-r3}
+        mov r0, r1              @r0 now contains dest
+        mov r1, r4              @puts src in r1
+        mov r2, r5              @puts len to be copy in r2        
+        bl arm_memcpy
+        pop {r0-r3}
+        
+    store_args_end:
+    
+    get_command_return:
+        mov r0, r3              @return command code
+    
+    
+    pop {r4, r5, r6, pc}
--- mc404/t02/part2/ra146446_p2.s
+++ mc404/t02/part2/ra146446_p2.s
@@ -3,6 +3,7 @@
 
 .globl arm_getline
 .globl print_string
+.globl arm_strtok
 
 .extern arm_strtou
 .extern arm_utostr
@@ -13,6 +14,9 @@
 .data
     read_line:  .space 100
     print_line: .space 100
+    cmd:        .space 100
+    arg1:       .space 100
+    arg2:       .space 100
     
 
 .text
@@ -20,7 +24,7 @@ arm_getline:
     push {r7, lr}
     mov r0, #0x0        @reads from stdin
     ldr r1, =read_line  @loads read line address
-    mov r2, #30        @reads a maximum of 100 bytes
+    mov r2, #100        @reads a maximum of 100 bytes
     mov r7, #0x3        @determines it is a read syscall
     svc 0               @make a syscall with previews parameters
     
@@ -45,3 +49,56 @@ print_string:
     ldr r0, =read_line  @returns the address of line read
     
     pop {r7, pc}
+
+arm_strtok:
+    push {r4, r5, lr}
+    
+    mov r4, #0                          @initialize r4 that counts words token
+    arm_strtok_loop:
+        ldrb r1, [r0], #1               @reads char and points to next
+        cmp r1, #' '                    @treats space
+        beq check_flag
+        change_flag:
+            mov r3, #0                  @changes flag
+            
+            cmp r1, #'\n'               @checks if line end
+            beq arm_strtok_loop_end     @stop processing
+            
+            cmp r4, #0                  @if its first word
+            beq store_cmd
+            cmp r4, #1                  @if its second word
+            beq store_arg1
+            cmp r4, #2                  @if its third word
+            beq store_arg2
+            store_cmd:
+                ldr r5, =cmd
+                str r0, [r5]
+                b store_break
+            store_arg1:
+                ldr r5, =arg1
+                str r0, [r5]
+                b store_break
+            store_arg2:
+               ldr r5, =arg2
+                str r0, [r5]
+            store_break:
+                add r4, r4, #1
+            b treat_tok_end             @process next
+        check_flag:
+            cmpeq r3, #1                @only if last wasn't space
+            beq treat_tok_end
+        
+        treat_tok:
+            mov r2, #0                  @puts '\0' when token is found
+            strb r2, [r1]               @puts '\0' when token is found
+            mov r3, #1                  @sets flag to treat spaces
+        treat_tok_end:
+        
+        b arm_strtok_loop
+    arm_strtok_loop_end:
+    
+    mov r0, r4
+    
+    pop {r4, r5, pc}
+    
+    
